1ba6dc3b930d2a421c72d62f988c2a1c68baa635
ponycui <PonyCui_Home@saitekiiMac.local>
2016-10-22 18:18:42 +0800

    Add onLayout. Add Slider.

---
+++ ios/runtime/components/js/Slider/UXKSLIDER.js
@@ -0,0 +1,3 @@
+<view vKey="backgroundView" frame="0,8,-1,4" backgroundColor="#dcdcdc" cornerRadius="14">
+	<view vKey="controlView" frame="2,2,24,24" backgroundColor="#ffffff" cornerRadius="12"></view>
+</view>
