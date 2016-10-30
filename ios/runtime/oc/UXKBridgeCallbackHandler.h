b0ed32ed03b82ec9c2afbc36b4161fb0befba8dd
ponycui <PonyCui_Home@saitekiiMac.local>
2016-10-30 15:06:18 +0800

    Recode callback.

---
+++ ios/runtime/oc/UXKBridgeCallbackHandler.h
@@ -0,0 +1,13 @@
+//
+//  UXKBridgeCallbackHandler.h
+//  uxkit
+//
+//  Created by 崔 明辉 on 16/10/30.
+//  Copyright © 2016年 YY Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UXKBridgeCallbackHandler : NSObject
+
+@end
---
+++ ios/runtime/oc/UXKBridgeCallbackHandler.m
@@ -0,0 +1,13 @@
+//
+//  UXKBridgeCallbackHandler.m
+//  uxkit
+//
+//  Created by 崔 明辉 on 16/10/30.
+//  Copyright © 2016年 YY Inc. All rights reserved.
+//
+
+#import "UXKBridgeCallbackHandler.h"
+
+@implementation UXKBridgeCallbackHandler
+
+@end
